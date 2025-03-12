ef6b2dfaa22f7bec4f5724c3c97fc40ef7d170ff
Noufal <mcnmjr@gmail.com>
2025-03-12 10:53:53 +0530

    Removed unused of importing

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import uvicorn
-from dotenv import load_dotenv
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
-
 @app.get("/cicd/v1/getHealth")
 def getHealthCheck()->str:
-    #Checking the health status
+    #Checking the health status for confirming
     return '{"Health":"OK"}'
-
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
