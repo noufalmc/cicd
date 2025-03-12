fa8e2052fda6f8d3deb7bc5c2fda61ed09c37d51
Noufal <mcnmjr@gmail.com>
2025-03-12 10:58:33 +0530

    flake8 removed

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
+#Checking the health status for confirming
 @app.get("/cicd/v1/getHealth")
 def getHealthCheck()->str:
-    #Checking the health status for confirming
     return '{"Health":"OK"}'
 
 if __name__ == "__main__":
