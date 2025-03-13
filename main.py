3ad096486c82dcc4e3d09c7c1ca45b8afa98e7e2
Noufal <mcnmjr@gmail.com>
2025-03-13 16:39:21 +0530

    new api addded

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 
+
 app = FastAPI()
-#Checking the health status for confirming
+
+
 @app.get("/cicd/v1/getHealth")
-def getHealthCheck()->str:
+def getHealthCheck() -> str:
+    # Checking the health status for confirming
     return '{"Health":"OK"}'
+
+@app.get("/cicd/v1/say_hello")
+def say_hello() -> str:
+    # Checking the health status for confirming
+    return '{"message":"Say Hello"}'
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
